03a8f4412d2345b7f0f322ad200af7077e11c632
Calvin Szeto <szeto.calvin@gmail.com>
2013-09-19 16:01:04 -0500

    Change state/population variables to franchise/games variables.

--- p1-nba-team-stacked-bar/nba-chart.js
+++ p1-nba-team-stacked-bar/nba-chart.js
@@ -1,15 +1,23 @@
+// Used for setting width/height of chart
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
 width = 960 - margin.left - margin.right,
 height = 500 - margin.top - margin.bottom;
 
+// https://github.com/mbostock/d3/wiki/Ordinal-Scales
+// Set the scale for the x axis
 var x = d3.scale.ordinal()
 .rangeRoundBands([0, width], .1);
+// rangeRoundBands has antialiasing: http://en.wikipedia.org/wiki/Spatial_anti-aliasing
 
+// https://github.com/mbostock/d3/wiki/Quantitative-Scales
 var y = d3.scale.linear()
 .rangeRound([height, 0]);
 
+// Green for wins, Red for losses
 var color = d3.scale.ordinal()
-.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+.range(["#00FF50", "#B20300"]);
+
+// https://github.com/mbostock/d3/wiki/SVG-Axes
 
 var xAxis = d3.svg.axis()
 .scale(x)
@@ -20,15 +28,19 @@ var yAxis = d3.svg.axis()
 .orient("left")
 .tickFormat(d3.format(".2s"));
 
+// Set up the HTML for what's about to happen!
 var svg = d3.select("body").append("svg")
 .attr("width", width + margin.left + margin.right)
 .attr("height", height + margin.top + margin.bottom)
 .append("g")
 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+// Grab the data
 d3.csv("data/nba_team_index.csv", function(error, data) {
-	color.domain(d3.keys(data[0]).filter(function(key) { return key !== "State"; }));
+	// Associate colors with the W and L columns
+	color.domain(d3.keys(data[0]).filter(function(key) { return key == "W" || key == "L"; }));
 
+	// What's happening here?
 	data.forEach(function(d) {
 		var y0 = 0;
 		d.ages = color.domain().map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; });
@@ -37,7 +49,7 @@ d3.csv("data/nba_team_index.csv", function(error, data) {
 
 	data.sort(function(a, b) { return b.total - a.total; });
 
-	x.domain(data.map(function(d) { return d.State; }));
+	x.domain(data.map(function(d) { return d.Franchise; }));
 	y.domain([0, d3.max(data, function(d) { return d.total; })]);
 
 	svg.append("g")
@@ -53,15 +65,15 @@ d3.csv("data/nba_team_index.csv", function(error, data) {
 	.attr("y", 6)
 	.attr("dy", ".71em")
 	.style("text-anchor", "end")
-	.text("Population");
+	.text("Games");
 
-	var state = svg.selectAll(".state")
+	var franchise = svg.selectAll(".franchise")
 	.data(data)
 	.enter().append("g")
 	.attr("class", "g")
-	.attr("transform", function(d) { return "translate(" + x(d.State) + ",0)"; });
+	.attr("transform", function(d) { return "translate(" + x(d.Franchise) + ",0)"; });
 
-	state.selectAll("rect")
+	franchise.selectAll("rect")
 	.data(function(d) { return d.ages; })
 	.enter().append("rect")
 	.attr("width", x.rangeBand())
